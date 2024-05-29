b332c060b3c1d4d89b7420a815c79778a1f02896
Andrei-David Nan <nandavid190@gmail.com>
2024-05-29 23:25:18 +0300

    v0.0.9 - set up home page;

--- mealie-frontend/src/AppRoutes.tsx
+++ mealie-frontend/src/AppRoutes.tsx
@@ -1,16 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
+import HomePage from './pages/HomePage';
 import Layout from './layouts/layout';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route 
-        path="/" 
-        element={
-          <Layout>
-            HOME PAGE
-          </Layout>} />
+      <Route path="/" element={<Layout><HomePage /></Layout>} />
       <Route path="/user-profile" element={<span>User Profile Page</span>} />
       <Route path="/about" element={<span>About Page</span>} />
       <Route path="*" element={<Navigate to="/" />} />
