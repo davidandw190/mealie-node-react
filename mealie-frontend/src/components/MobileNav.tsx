2be08a4fd8bf2bdbdd85923fc1361f9a805f54e5
Andrei-David Nan <nandavid190@gmail.com>
2024-06-01 00:18:09 +0300

    v0.1.4 - set up logged in user auth mobile submenu menu and logout;

--- mealie-frontend/src/components/MobileNav.tsx
+++ mealie-frontend/src/components/MobileNav.tsx
@@ -1,10 +1,13 @@
+import { CircleUserRound, Menu } from 'lucide-react';
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from './ui/sheet';
 
 import { Button } from './ui/button';
-import { Menu } from 'lucide-react';
+import MobileNavLinks from './MobileNavLinks';
 import { Separator } from './ui/separator';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const MobileNav: React.FC = () => {
+  const { isAuthenticated, loginWithRedirect, user} = useAuth0();
   return (
     <Sheet>
       <SheetTrigger>
@@ -13,11 +16,21 @@ const MobileNav: React.FC = () => {
 
       <SheetContent className='space-y-3'>
         <SheetTitle>
-          <span> Welcome to Mealie.io!</span>
+          {isAuthenticated ? (
+            <span className='flex items-center font-bold gap-2'>
+              <CircleUserRound className='text-orange-500' /> {user?.email}
+            </span>
+          ) : (
+            <span> Welcome to Mealie.io!</span>
+          )}
         </SheetTitle>
         <Separator />
-        <SheetDescription className='flex'>
-          <Button className='flex-1 font-bold bg-orange-500'>Log In</Button>
+        <SheetDescription className='flex flex-col gap-4'>
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button onClick={() => loginWithRedirect()} className='flex-1 font-bold bg-orange-500'>Log In</Button>
+          )}
         </SheetDescription>
       </SheetContent>
     </Sheet>
---
+++ mealie-frontend/src/components/MobileNavLinks.tsx
@@ -0,0 +1,39 @@
+import { Button } from './ui/button';
+import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const MobileNavLinks: React.FC = () => {
+  const { logout } = useAuth0();
+  
+  return (
+    <>
+      <Link
+        to='/manage-restaurant'
+        className='flex bg-white items-center font-bold hover:text-orange-500'
+      >
+        Manage Restaurant
+      </Link>
+
+      <Link
+        to='/order-status'
+        className='flex bg-white items-center font-bold hover:text-orange-500'
+      >
+        Order Status
+      </Link>
+      <Link
+        to='/user-profile'
+        className='flex bg-white items-center font-bold hover:text-orange-500'
+      >
+        User Profile
+      </Link>
+      <Button
+        onClick={() => logout()}
+        className='flex items-center px-3 font-bold hover:bg-gray-500'
+      >
+        Log Out
+      </Button>
+    </>
+  );
+};
+
+export default MobileNavLinks;
