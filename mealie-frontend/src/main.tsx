7d706418057366a90a59a3ba72bba358d584d840
Andrei-David Nan <nandavid190@gmail.com>
2024-06-01 13:18:44 +0300

    v0.1.7 - integrate react query, set up query client provider;

--- mealie-frontend/src/main.tsx
+++ mealie-frontend/src/main.tsx
@@ -1,17 +1,29 @@
 import './global.css';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import AppRoutes from './AppRoutes';
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <Auth0ProviderWithNavigate>
-        <AppRoutes />
-      </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
 );
