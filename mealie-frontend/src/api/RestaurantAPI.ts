f0e6d96bd2fed6de68353e1b9732afff9169284f
Andrei-David Nan <nandavid190@gmail.com>
2024-06-08 14:15:10 +0300

    v0.3.17 - configure owned restaurat details retrieval in frontend;

--- mealie-frontend/src/api/RestaurantAPI.ts
+++ mealie-frontend/src/api/RestaurantAPI.ts
@@ -1,16 +1,48 @@
-import { Restaurant } from "../types/types";
-import { toast } from "sonner";
-import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from 'react-query';
+
+import { Restaurant } from '../types/types';
+import { toast } from 'sonner';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useRetrieveOwnedRestuarant = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const retrieveOwnedRestaurantRequest = async (): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/restaurants/owned`, {
+      headers: {
+        method: 'GET',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to retrieve owned restaurant');
+    }
+
+    return response.json();
+  };
+
+  const { data: ownedRestaurant, isLoading, isSuccess, error } = useQuery('fetchOwnedRestaurant', retrieveOwnedRestaurantRequest);
+  
+  if (error) {
+    console.error(error);
+    toast.error('Failed to retrieve owned restaurant');
+  }
+
+  return { ownedRestaurant, isLoading, isSuccess };
+};
+
 
 export const useRegisterRestaurant = () => {
   const { getAccessTokenSilently } = useAuth0();
 
   const createRestaurantRequest = async (restaurantFormData: FormData): Promise<Restaurant> => {
     const accessToken = await getAccessTokenSilently();
-    
+
     const response = await fetch(`${API_BASE_URL}/api/restaurants/owned`, {
       method: 'POST',
       headers: {
@@ -26,7 +58,12 @@ export const useRegisterRestaurant = () => {
     return response.json();
   };
 
-  const { mutate: registerRestaurant, isLoading, isSuccess, error} = useMutation(createRestaurantRequest);
+  const {
+    mutate: registerRestaurant,
+    isLoading,
+    isSuccess,
+    error,
+  } = useMutation(createRestaurantRequest);
 
   if (error) {
     console.error(error);
@@ -34,4 +71,4 @@ export const useRegisterRestaurant = () => {
   }
 
   return { registerRestaurant, isLoading, isSuccess };
-}
+};
