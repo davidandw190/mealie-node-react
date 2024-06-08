4cb4deefc7f9f4af08504cc6152c996250d04b6d
Andrei-David Nan <nandavid190@gmail.com>
2024-06-08 15:36:57 +0300

    v0.3.19 - small issues fixed regarding manage restaurant functionality;

--- mealie-frontend/src/api/RestaurantAPI.ts
+++ mealie-frontend/src/api/RestaurantAPI.ts
@@ -6,7 +6,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const useRetrieveOwnedRestuarant = () => {
+export const useRetrieveOwnedRestaurant = () => {
   const { getAccessTokenSilently } = useAuth0();
 
   const retrieveOwnedRestaurantRequest = async (): Promise<Restaurant> => {
--- mealie-frontend/src/pages/ManageRestaurantPage.tsx
+++ mealie-frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,12 +1,20 @@
-import { useRegisterRestaurant, useRetrieveOwnedRestuarant } from '@/api/RestaurantAPI';
+import { useRegisterRestaurant, useRetrieveOwnedRestaurant } from '@/api/RestaurantAPI';
 
 import ManageRestaurantForm from '@/forms/manage-restaurant-form/ManageRestaurantForm';
 
 const ManageRestaurantPage = () => {
-  const { ownedRestaurant, isLoading: fetchIsLoading } = useRetrieveOwnedRestuarant();
+  const { ownedRestaurant, isLoading: fetchIsLoading } = useRetrieveOwnedRestaurant();
   const { registerRestaurant, isLoading: postIsLoading } = useRegisterRestaurant();
 
-  return <ManageRestaurantForm onSave={registerRestaurant} isLoading={postIsLoading} />;
+  return fetchIsLoading ? (
+    <div>Loading...</div>
+  ) : (
+    <ManageRestaurantForm
+      onSave={registerRestaurant}
+      isLoading={postIsLoading}
+      ownedRestaurant={ownedRestaurant}
+    />
+  );
 };
 
 export default ManageRestaurantPage;
