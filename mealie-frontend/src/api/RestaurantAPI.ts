d52b90ebd5d54888835508dc01cdb7bdec4625f5
Andrei-David Nan <nandavid190@gmail.com>
2024-06-12 14:19:19 +0300

    v0.4.12 - add the current page query param to the search restaurant query;

--- mealie-frontend/src/api/RestaurantAPI.ts
+++ mealie-frontend/src/api/RestaurantAPI.ts
@@ -120,6 +120,7 @@ export const useSearchRestaurants = (searchState: SearchState, city?: string) =>
   const createSearchRequest = async (): Promise<RestaurantSearchResponse> => {
     const params = new URLSearchParams();
     params.set('searchQuery', searchState.searchQuery);
+    params.set('page', searchState.page.toString())
     const response = await fetch(
       `${API_BASE_URL}/api/restaurants/search/${city}?${params.toString()}`,
       {
