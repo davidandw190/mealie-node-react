a341e391dd692b0dd1538cfcfc87b71f675f4383
Andrei-David Nan <nandavid190@gmail.com>
2024-06-12 16:00:39 +0300

    v0.4.14 - implement cuisines list expansion feature, set up handleCuisinesChange;

--- mealie-frontend/src/components/CuisineFilter.tsx
+++ mealie-frontend/src/components/CuisineFilter.tsx
@@ -1,21 +1,30 @@
+import { Check, ChevronDown, ChevronUp } from 'lucide-react';
+
+import { Button } from './ui/button';
 import { ChangeEvent } from 'react';
-import { Check } from 'lucide-react';
-import { Label } from '@radix-ui/react-label';
+import { Label } from './ui/label';
 import { cuisinesList } from '../../config/restaurant-options.config';
 
 type Props = {
   onChange: (cuisines: string[]) => void;
   selectedCuisines: string[];
   isExpanded: boolean;
-  onExpandedClicked: () => void;
+  onExpandedClick: () => void;
 };
 
-const CuisineFilter = ({ onChange, selectedCuisines, isExpanded, onExpandedClicked }: Props) => {
-  const handleCuisinesReset = () => onChange([]);
+const CuisineFilter = ({ onChange, selectedCuisines, isExpanded, onExpandedClick }: Props) => {
+  const handleCuisinesChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const clickedCuisine = event.target.value;
+    const isChecked = event.target.checked;
 
-  function handleCuisinesChange(event: ChangeEvent<HTMLInputElement>): void {
-    throw new Error('Function not implemented.');
-  }
+    const newCuisinesList = isChecked
+      ? [...selectedCuisines, clickedCuisine]
+      : selectedCuisines.filter(cuisine => cuisine !== clickedCuisine);
+
+    onChange(newCuisinesList);
+  };
+
+  const handleCuisinesReset = () => onChange([]);
 
   return (
     <>
@@ -30,11 +39,10 @@ const CuisineFilter = ({ onChange, selectedCuisines, isExpanded, onExpandedClick
       </div>
 
       <div className='space-y-2 flex flex-col'>
-        {cuisinesList.map(cuisine => {
+        {cuisinesList.slice(0, isExpanded ? cuisinesList.length : 7).map(cuisine => {
           const isSelected = selectedCuisines.includes(cuisine);
-
           return (
-            <div>
+            <div className='flex'>
               <input
                 id={`cuisine_${cuisine}`}
                 type='checkbox'
@@ -55,6 +63,18 @@ const CuisineFilter = ({ onChange, selectedCuisines, isExpanded, onExpandedClick
             </div>
           );
         })}
+
+        <Button onClick={onExpandedClick} variant='link' className='mt-4 flex-1'>
+          {isExpanded ? (
+            <span className='flex flex-row items-center'>
+              View Less <ChevronUp />
+            </span>
+          ) : (
+            <span className='flex flex-row items-center'>
+              View More <ChevronDown />
+            </span>
+          )}
+        </Button>
       </div>
     </>
   );
