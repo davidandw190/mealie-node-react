e8cfa962b2ddaf477dc71019d76dbe060b2e040e
Andrei-David Nan <nandavid190@gmail.com>
2024-06-12 16:16:09 +0300

    v0.4.15 - finish state management for cuisines filter;

--- mealie-frontend/src/pages/SearchPage.tsx
+++ mealie-frontend/src/pages/SearchPage.tsx
@@ -1,5 +1,6 @@
 import SearchBar, { SearchForm } from '@/components/SearchBar';
 
+import CuisineFilter from '@/components/CuisineFilter';
 import PaginationSelector from '@/components/PaginationSelector';
 import SearchResultCard from '@/components/SearchResultCard';
 import SearchResultsInfo from '@/components/SearchResultsInfo';
@@ -10,13 +11,19 @@ import { useState } from 'react';
 export type SearchState = {
   searchQuery: string;
   page: number;
-
+  selectedCuisines: string[];
 };
 
 const SearchPage = () => {
   const { city } = useParams();
-  const [searchState, setSearchState] = useState<SearchState>({ searchQuery: '', page: 1 });
-  
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: '',
+    page: 1,
+    selectedCuisines: [],
+  });
+
+  const [isCuisinesExpanded, setIsCuisinesExpanded] = useState<boolean>(false);
+
   const { results, isLoading } = useSearchRestaurants(searchState, city);
 
   const setSearchQuery = (seachForm: SearchForm) => {
@@ -27,12 +34,20 @@ const SearchPage = () => {
     }));
   };
 
+  const setSelectedCuisines = (selectedCuisines: string[]) => {
+    setSearchState(prevState => ({
+      ...prevState,
+      selectedCuisines,
+      page: 1,
+    }));
+  };
+
   const setPage = (page: number) => {
     setSearchState(prevState => ({
       ...prevState,
       page,
     }));
-  }
+  };
 
   const resetSearch = () => {
     setSearchState(prevState => ({
@@ -52,8 +67,14 @@ const SearchPage = () => {
 
   return (
     <div className='grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5'>
-      
-      <div id='cuisines-list'>TODO: INSERT CUISINES LIST HERE</div>
+      <div id='cuisines-list'>
+        <CuisineFilter
+          selectedCuisines={searchState.selectedCuisines}
+          onChange={setSelectedCuisines}
+          isExpanded={isCuisinesExpanded}
+          onExpandedClick={() => setIsCuisinesExpanded(previousState => !previousState)}
+        />
+      </div>
       <div id='main-content' className='flex flex-col gap-5'>
         <SearchBar
           onSubmit={setSearchQuery}
@@ -61,12 +82,12 @@ const SearchPage = () => {
           searchQuery={searchState.searchQuery}
           placeholder='Search by cuisine or restaurant name'
         />
-       
+
         <SearchResultsInfo total={results.pagination.total} city={city} />
         {results.data.map(restaurant => (
           <SearchResultCard restaurant={restaurant} key={restaurant._id} />
         ))}
-        <PaginationSelector 
+        <PaginationSelector
           page={results.pagination.page}
           pages={results.pagination.pages}
           onPageChange={setPage}
