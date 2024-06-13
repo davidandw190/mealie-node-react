437932f3abfd3f5202b5b3dc5acb2d77eb419aec
Andrei-David Nan <nandavid190@gmail.com>
2024-06-13 14:13:18 +0300

    v0.5.2 - set up and integrate MenuItemCard;

---
+++ mealie-frontend/src/components/MenuItemCard.tsx
@@ -0,0 +1,23 @@
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
+
+import { MenuItem } from '../types/types';
+
+type Props = {
+  menuItem: MenuItem;
+  addToCart: () => void;
+};
+
+const MenuItemCard = ({ menuItem, addToCart }: Props) => {
+  return (
+    <Card className='cursor-pointer' onClick={addToCart}>
+      <CardHeader>
+        <CardTitle>{menuItem.name}</CardTitle>
+      </CardHeader>
+      <CardContent className="font-bold">
+        £{(menuItem.price / 100).toFixed(2)}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MenuItemCard
