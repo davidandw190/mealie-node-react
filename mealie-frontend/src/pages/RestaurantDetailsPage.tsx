3b31ca0f35afde3b552484756c864247c514519d
Andrei-David Nan <nandavid190@gmail.com>
2024-06-13 15:30:09 +0300

    v0.5.4 - setup and integrate RestaurantInfoCard, start work on cart state handling ;

--- mealie-frontend/src/pages/RestaurantDetailsPage.tsx
+++ mealie-frontend/src/pages/RestaurantDetailsPage.tsx
@@ -3,12 +3,27 @@ import { Card, CardFooter } from '@/components/ui/card';
 import { AspectRatio } from '@radix-ui/react-aspect-ratio';
 import { MenuItem } from '@/types/types';
 import MenuItemCard from '@/components/MenuItemCard';
+import RestaurantInfo from '@/components/RestaurantInfo';
 import { useParams } from 'react-router-dom';
 import { useRetrieveRestaurantDetails } from '@/api/RestaurantAPI';
+import { useState } from 'react';
+
+export type CartItem = {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
 
 const RestaurantDetailsPage = () => {
   const { restaurantId } = useParams();
   const { restaurant, isLoading } = useRetrieveRestaurantDetails(restaurantId);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+
+  if (isLoading || !restaurant) {
+    return "Loading...";
+  }
 
   return (
     <div className='flex flex-col gap-10'>
@@ -17,7 +32,7 @@ const RestaurantDetailsPage = () => {
       </AspectRatio>
       <div className='grid md:grid-cols-[4fr_2fr] gap-5 md:px-32'>
         <div className='flex flex-col gap-4'>
-          <span>RESTAURANT INFO HERE</span>
+          <RestaurantInfo restaurant={restaurant} />
           <span className='text-2xl font-bold tracking-tight'>Menu</span>
           {restaurant?.menuItems.map((menuItem: MenuItem) => (
             <MenuItemCard menuItem={menuItem} addToCart={() => console.log(menuItem.name)} />
